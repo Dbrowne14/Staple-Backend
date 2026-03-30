8d6eae71a107c77303b86fcfcfbd508f267f3a8f
Dbrowne14 <davidbrowne1992@gmail.com>
2026-03-30 17:51:38 +0100

    populating the db

--- src/index.ts
+++ src/index.ts
@@ -6,19 +6,21 @@ import {
   handleTypeLine,
   handleYear,
   getImg,
-  fetchTopCards
+  fetchTopCards,
 } from "./apiObjectLogic";
 import type { ReturnStructure } from "./types/types";
 
 const pool = new Pool({
-  user: 'davidbrowne',
-  host: 'localhost',
-  database: 'Staple_db',
-  port: 5432
+  user: "davidbrowne",
+  host: "localhost",
+  database: "Staple_db",
+  port: 5432,
 });
 
 (async () => {
-  const res = await pool.query('SELECT NOW()');
+  const res = await pool.query(
+    `SELECT COUNT(*) as column_count FROM information_schema.columns WHERE table_name = 'cards'`,
+  );
   console.log(res.rows);
 })();
 
@@ -52,6 +54,43 @@ app.get("/cards/search", async (req: Request, res: ExpressResponse) => {
       Colors: card.color_identity.length,
       Rank: card.edhrec_rank,
     }));
+    for (const card of returnObject) {
+      const {
+        ScryFall_id,
+        Name,
+        CMC,
+        Type: { type, legendary },
+        Img,
+        Year,
+        Rarity,
+        Set: { set, setName },
+        Price,
+        Pips,
+        Colors,
+        Rank,
+      } = card;
+
+      await pool.query(
+        `INSERT INTO cards(scryfall_id, name, cmc, type, islegendary, img, year, rarity, set_code, set_name, price, pips, colors, edhrec_rank)
+      VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14) ON CONFLICT (scryfall_id) DO NOTHING`,
+        [
+          ScryFall_id,
+          Name,
+          CMC,
+          type,
+          legendary,
+          Img,
+          Year,
+          Rarity,
+          set,
+          setName,
+          Price,
+          Pips,
+          Colors,
+          Rank,
+        ],
+      );
+    }
 
     res.json(returnObject);
   } catch (err) {
