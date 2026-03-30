81887e0dd035de40aa7acf575b2748efe6f5e71a
Dbrowne14 <davidbrowne1992@gmail.com>
2026-03-30 18:30:29 +0100

    adding cron call to index.ts

---
+++ src/cronCalls.ts
@@ -0,0 +1,81 @@
+import {
+  handlePips,
+  handlePrice,
+  handleTypeLine,
+  handleYear,
+  getImg,
+  fetchTopCards,
+} from "./apiObjectLogic";
+import { cardsLimit } from "./data/inputData";
+
+import type { ReturnStructure } from "./types/types";
+
+import { Pool } from "pg";
+
+const pool = new Pool({
+  user: "davidbrowne",
+  host: "localhost",
+  database: "Staple_db",
+  port: 5432,
+});
+
+export const updateDatabase = async () => {
+  const rawData = await fetchTopCards(cardsLimit);
+
+  //predefined object based on structure of the game
+  const returnObject = rawData.map((card: ReturnStructure) => ({
+    ScryFall_id: card.id,
+    Name: card.name,
+    CMC: card.cmc,
+    Type: handleTypeLine(card),
+    Img: getImg(card),
+    Year: handleYear(card),
+    Rarity: card.rarity,
+    Set: {
+      set: card.set,
+      setName: card.set_name,
+    },
+    Price: handlePrice(card),
+    Pips: handlePips(card),
+    Colors: card.color_identity.length,
+    Rank: card.edhrec_rank,
+  }));
+
+  for (const card of returnObject) {
+    const {
+      ScryFall_id,
+      Name,
+      CMC,
+      Type: { type, legendary },
+      Img,
+      Year,
+      Rarity,
+      Set: { set, setName },
+      Price,
+      Pips,
+      Colors,
+      Rank,
+    } = card;
+
+    await pool.query(
+      `INSERT INTO cards(scryfall_id, name, cmc, type, islegendary, img, year, rarity, set_code, set_name, price, pips, colors, edhrec_rank)
+          VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14) ON CONFLICT (scryfall_id) DO NOTHING`,
+      [
+        ScryFall_id,
+        Name,
+        CMC,
+        type,
+        legendary,
+        Img,
+        Year,
+        Rarity,
+        set,
+        setName,
+        Price,
+        Pips,
+        Colors,
+        Rank,
+      ],
+    );
+  }
+};
--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,9 @@
 import express, { Request, Response as ExpressResponse } from "express";
 import { Pool } from "pg";
-import {
-  handlePips,
-  handlePrice,
-  handleTypeLine,
-  handleYear,
-  getImg,
-  fetchTopCards,
-} from "./apiObjectLogic";
+import { cardsLimit } from "./data/inputData";
+const cron = require("node-cron");
+import { updateDatabase } from "./cronCalls";
+
 import type { ReturnStructure } from "./types/types";
 
 const pool = new Pool({
@@ -25,79 +21,14 @@ const pool = new Pool({
 })();
 
 const app = express();
-const cardsLimit = 1000;
 
-//get request for the dataset
-app.get("/cards/search", async (req: Request, res: ExpressResponse) => {
-  if (!req.query) {
-    return res.status(400).json({ error: "invalid search" });
-  }
-
+//run cron call for weekly data update runs every Sunday
+cron.schedule("0 0 0 * * 0", async () => {
   try {
-    const rawData = await fetchTopCards(cardsLimit);
-
-    //predefined object based on structure of the game
-    const returnObject = rawData.map((card: ReturnStructure) => ({
-      ScryFall_id: card.id,
-      Name: card.name,
-      CMC: card.cmc,
-      Type: handleTypeLine(card),
-      Img: getImg(card),
-      Year: handleYear(card),
-      Rarity: card.rarity,
-      Set: {
-        set: card.set,
-        setName: card.set_name,
-      },
-      Price: handlePrice(card),
-      Pips: handlePips(card),
-      Colors: card.color_identity.length,
-      Rank: card.edhrec_rank,
-    }));
-    
-    for (const card of returnObject) {
-      const {
-        ScryFall_id,
-        Name,
-        CMC,
-        Type: { type, legendary },
-        Img,
-        Year,
-        Rarity,
-        Set: { set, setName },
-        Price,
-        Pips,
-        Colors,
-        Rank,
-      } = card;
-
-      await pool.query(
-        `INSERT INTO cards(scryfall_id, name, cmc, type, islegendary, img, year, rarity, set_code, set_name, price, pips, colors, edhrec_rank)
-      VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14) ON CONFLICT (scryfall_id) DO NOTHING`,
-        [
-          ScryFall_id,
-          Name,
-          CMC,
-          type,
-          legendary,
-          Img,
-          Year,
-          Rarity,
-          set,
-          setName,
-          Price,
-          Pips,
-          Colors,
-          Rank,
-        ],
-      );
-    }
-
-    res.json(returnObject);
+    console.log("Starting weekly db update");
+    await updateDatabase();
   } catch (err) {
-    return res
-      .status(500)
-      .json({ error: "Internal server error", details: err });
+    console.error("Failed to update cards:", err);
   }
 });
 
